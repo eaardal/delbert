f3d62cddc9cbd40e09763b3851d050ae7da70a4d
Eirik Årdal <eaardal@outlook.com>
2015-08-31 07:51:03 +0200

    Started implementing basic NotebookActor logic

---
+++ Delbert/Services/NotebookActor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Delbert.Infrastructure;
+using Delbert.Infrastructure.Logging.Contracts;
+
+namespace Delbert.Services
+{
+    internal class NotebookActor : LoggingReceiveActor
+    {
+        public NotebookActor(ILogger logger) : base(logger)
+        {
+            Receive<CreateNotebook>(msg => OnCreateNotebook(msg));
+        }
+
+        private void OnCreateNotebook(CreateNotebook message)
+        {
+            var directory = message.Directory;
+
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+        }
+
+        internal class CreateNotebook
+        {
+            public DirectoryInfo Directory { get; }
+
+            public CreateNotebook(DirectoryInfo directory)
+            {
+                if (directory == null) throw new ArgumentNullException(nameof(directory));
+                Directory = directory;
+            }
+        }
+    }
+
+    
+}
